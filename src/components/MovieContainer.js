d2fb53db1d5f72e013572830f9c8742b80c8216e
Leonardo Plaza <leoplazavzla@gmail.com>
2022-08-25 16:43:36 -0300

    Added some styles for the movie container

--- src/components/MovieContainer.js
+++ src/components/MovieContainer.js
@@ -2,4 +2,17 @@ import styled from "styled-components";
 
 export const MovieContainer = styled.div`
     display: flex;
+    flex-direction: column;
+    align-items: center; 
+    background-color: grey;
+    border-radius: 3%;
+    width: 200px;
+    height: 300px;
+
+    .img{
+        margin: 20px 0px;
+        width 180px;
+        height: 150px;
+        border: 2px solid black;
+    }
 `
--- src/pages/MovieDetail.js
+++ src/pages/MovieDetail.js
@@ -1,6 +1,26 @@
 import React, {useState} from "react";
+import { MovieContainer } from "../components/MovieContainer";
 
 const MovieDetail = () => {
+    return(
+        <>
+        <h1>MovieDetail</h1>
+        <MovieContainer>
+            <div className="stream">
+                <div className="img">
+
+                </div>
+                <div className="title">
+
+                </div>
+                <div className="year">
+
+                </div>
+            </div>
+
+        </MovieContainer>
+        </>
+    )
 
 }
 
