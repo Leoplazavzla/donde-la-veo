83e813e8ea857d781f03222db87cf099ab869048
leoplazavzla <Caracter$$123>
2023-01-16 11:40:25 -0300

    - Added responsiveness to the homepage grid

--- src/components/MoviesOrSeriesDataGrid.js
+++ src/components/MoviesOrSeriesDataGrid.js
@@ -2,10 +2,17 @@ import styled from "styled-components";
 
 export const MoviesOrSeriesDataGrid = styled.div`
     width: 90%;
-    max-width: 1800px;
+    max-width: 1200px;
     margin: 40px auto 100px auto;
     
     display: grid;
     grid-template-columns: repeat(4, 1fr);
     gap: 40px;
+    
+    @media (max-width: 680px){
+        grid-template-columns: 1fr;
+        grid-template-rows: 0.4fr 0.4fr;
+    }
+    
+    
 `
