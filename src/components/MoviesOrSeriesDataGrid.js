93dd0817ccc42df7845854db711743a353ee3af1
leoplazavzla <Caracter$$123>
2023-01-16 13:45:08 -0300

    - Fixed a style bug in the home grid

--- src/components/MoviesOrSeriesDataGrid.js
+++ src/components/MoviesOrSeriesDataGrid.js
@@ -3,7 +3,7 @@ import styled from "styled-components";
 export const MoviesOrSeriesDataGrid = styled.div`
     width: 90%;
     max-width: 1200px;
-    margin: 40px auto 100px auto;
+    margin: 40px auto 0px auto;
     
     display: grid;
     grid-template-columns: repeat(4, 1fr);
